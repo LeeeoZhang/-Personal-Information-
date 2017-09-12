44c26314a638a3fa5ce3c5a961d46d4f237d5a86
leo <leozhang621@gmail.com>
2017-09-12 21:57:58 +0800

    add nav

--- index.js
+++ index.js
@@ -1,48 +1,73 @@
 !function () {
     class FullPage {
-        constructor(pageContainer, duration) {
+        constructor (pageContainer, navContainer, duration) {
             this.pageContainer = pageContainer
             this.duration = duration
+            this.navContainer = navContainer
             this.currentIndex = 0
             this.isAnimate = false
             this.targetIndex = 0
             this.bindEvents()
         }
 
-        bindEvents() {
+        bindEvents () {
             this.pageContainer.addEventListener('wheel', (e) => {
                 this.targetIndex = this.currentIndex + (e.deltaY > 0 ? 1 : -1)
-                this.gotoSection(this.targetIndex).then(() => {
-                    this.currentIndex = this.targetIndex
+                this.gotoSection().then((targetIndex) => {
                     this.isAnimate = false
+                    this.currentIndex = targetIndex || this.targetIndex
+                    if(targetIndex === 0) this.currentIndex = 0
+                    console.log(this.currentIndex, this.targetIndex)
                 }).catch((error) => {
                     console.log(error)
                 })
             })
+            this.navEvent()
         }
 
-        gotoSection() {
+        gotoSection (index) {
             let _this = this
+            let targetIndex = index || this.targetIndex
+            if(index === 0) targetIndex = 0
             return new Promise((resolve, reject) => {
                 if (this.isAnimate) {
                     return reject()
-                } else if (this.targetIndex < 0) {
+                } else if (targetIndex < 0) {
                     return reject()
-                } else if (this.targetIndex >= this.pageContainer.children.length) {
+                } else if (targetIndex >= this.pageContainer.children.length) {
                     return reject()
                 }
                 this.isAnimate = true
-                this.pageContainer.children[0].addEventListener('transitionend', function callback() {
+                this.pageContainer.children[0].addEventListener('transitionend', function callback () {
                     _this.pageContainer.children[0].removeEventListener('transitionend', callback)
-                    resolve()
+                    resolve(targetIndex)
                 })
                 for (let i = 0; i < this.pageContainer.children.length; i++) {
-                    this.pageContainer.children[i].style.transform = `translateY(-${100 * this.targetIndex}%)`
+                    this.pageContainer.children[i].style.transform = `translateY(-${100 * targetIndex}%)`
+                }
+            })
+        }
+
+        navEvent () {
+            this.navContainer.addEventListener('click', (event) => {
+                if (event.target.nodeName.toLowerCase() === 'li') {
+                    let target = event.target
+                    let navList = this.navContainer.querySelectorAll('ul>li')
+                    let index = [].indexOf.call(navList, target)
+                    if(index === this.currentIndex) return
+                    this.gotoSection(index).then((targetIndex) => {
+                        this.isAnimate = false
+                        this.currentIndex = targetIndex || this.targetIndex
+                        if(targetIndex === 0) this.currentIndex = 0
+                    }).catch((error) => {
+                        console.log(error)
+                    })
                 }
             })
         }
     }
-    new FullPage(document.querySelector('.page'))
+
+    new FullPage(document.querySelector('.page'), document.querySelector('#navContainer'))
 }()
 
 
